e6e519f7615458ee43929ab29f30621793064dac
wpex <brunorochabezerra58@gmail.com>
2024-09-02 12:16:32 -0300

    API finalizada e testada

--- api.py
+++ api.py
@@ -10,34 +10,53 @@ class Movie(db.Model):
     title = db.Column(db.String(100), nullable=False) 
     genre = db.Column(db.String(100), nullable=False)  
 
-
 with app.app_context():
     db.create_all()
-
-movies = [
-    {'id':1, 'title': 'Star Wars', 'genre': 'scifi'},
-    {'id':2, 'title': 'It', 'genre': 'horror'},
-    {'id':3, 'title': 'Um tira da pesada', 'genre': 'comedy'}
-]
 
 @app.route('/api/movies', methods=['GET'])
 def get_movies():
     movies = Movie.query.all()
-    return jsonify(movies)
+    return jsonify([{"id":movie.id, "title":movie.title, "genre":movie.genre} for movie in movies])
 
 @app.route('/api/movies/<int:id>', methods=['GET'])
 def get_movie(id):
-    movie = next(
-        (movie for movie in movies if movie['id'] == id),
-        None
-    )
-    return jsonify(movie) if movie else ('', 404)
+    movie = Movie.query.get(id)
+    return jsonify({"id":movie.id, "title":movie.title, "genre":movie.genre}) if movie else ('', 404)
 
 @app.route('/api/movies', methods=['POST'])
 def add_movie():
-    new_movie = request.get_json()
-    movies.append(new_movie)
-    return jsonify(new_movie), 201
+    data = request.get_json()
+    new_movie = Movie(title=data['title'], genre=data['genre'])
+    db.session.add(new_movie)
+    db.session.commit()
+    return jsonify({"id":new_movie.id, "title":new_movie.title, "genre":new_movie.genre}), 201
 
-if __name__ == '__main__':
+@app.route('/api/movies/<int:id>', methods=['DELETE'])
+def delete_movie(id):
+    movie = Movie.query.get(id)
+    if movie is None:
+        return jsonify({"error":"movie not found"}), 404
+    
+    db.session.delete(movie)
+    db.session.commit()
+    return '',204
+
+@app.route('/api/movies/<int:id>', methods=['PUT'])
+def update_movies(id):
+    movie = Movie.query.get(id)
+    if movie is None:
+        return jsonify({"error":"movie not found"}), 404
+    
+    data = request.get_json()
+    movie.title = data.get('title', movie.title)
+    movie.genre = data.get('genre', movie.title)
+
+    db.session.commit()
+    return jsonify({
+        "id":movie.id,
+        "title":movie.title,
+        "genre":movie.genre
+    })
+
+if __name__== '__main__':
     app.run(port=5000, debug=True)
